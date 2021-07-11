db1e8e33037fda528c756286ccf3aa7c17da5dcc
vivekverma-ally <85276140+vivekverma-ally@users.noreply.github.com>
2021-07-11 08:56:14 +0530

    Update productDetails.js

--- assets/js/productDetails.js
+++ assets/js/productDetails.js
@@ -4,6 +4,7 @@ var currentObj = null
 fetch(`https://5d76bf96515d1a0014085cf9.mockapi.io/product/${product_id}`).then(res => res.json()).then(productData => {
 
     // Add Name
+    document.getElementById("productView").setAttribute("img", productData.photos[0])
     document.getElementsByTagName("h1")[0].innerHTML = productData.name;
     // Add Brand Name
     document.getElementsByTagName("h4")[0].innerHTML = productData.brand;
@@ -41,38 +42,17 @@ fetch(`https://5d76bf96515d1a0014085cf9.mockapi.io/product/${product_id}`).then(
             document.getElementById("productView").setAttribute("src", item);
             activeImg = `img${i}`;
         });
-    });
 
-    // currentObj = productData;
-    // var main_image = document.getElementById("main-image");
-    // main_image.src = productData.photos[0];
-    // var product_name = document.getElementById("product-name");
-    // product_name.textContent = productData.name;
-    // var product_brand = document.getElementById("product-brand");
-    // product_brand.textContent = productData.brand;
-    // var price_color = document.getElementById("price-color");
-    // price_color.textContent = productData.price;
-    // var product_description = document.getElementById("product-description");
-    // product_description.textContent = productData.description;
-    // var product_images = document.getElementsByClassName("product-images");
-    // var product_images = product_images[0];
-    // for (var i = 0; i < productData.photos.length; i++) {
-    //     var image_div = document.createElement("img");
-    //     image_div.src = productData.photos[i];
-    //     image_div.id = "product-image-" + i;
-    //     image_div.className = "clickable";
-    //     image_div.onclick = imgClick;
-    //     product_images.appendChild(image_div);
-    // }
-    // function imgClick() {
-    //     imgClicked = document.getElementById(this.id);
-    //     clickable_images = document.getElementsByClassName("clickable");
-    //     for (var img of clickable_images) {
-    //         img.classList.remove("clicked");
-    //     }
-    //     imgClicked.classList.add("clicked");
-    //     main_image.src = imgClicked.src;
-    // }
+    });
+    var buy = document.createElement("input");
+    buy.setAttribute("type", "button");
+    buy.setAttribute("value", "Add to Cart");
+    buy.classList.add("buy-button");
+    buy.addEventListener("click", () => {
+        window.alert(`${productData.name} is added to your cart`);
+    });
+    productDetails.appendChild(buy);
+
 });
 var productList = window.localStorage.getItem('product-list');
 productList = productList === null || productList === '' ? [] : productList;
@@ -116,65 +96,3 @@ $("#add-to-cart").click(function () {
     }
     $('#cart-total').html(totalCount);
 });
-
-
-
-// var productData = sessionStorage.getItem("item");
-
-
-// console.log(productData == null);
-
-
-// var productDetails = document.getElementById("productDetails");
-
-// console.log(productData.length);
-
-// // Add Name
-// document.getElementsByTagName("h1")[0].innerHTML = productData.name;
-// // Add Brand Name
-// document.getElementsByTagName("h4")[0].innerHTML = productData.brand;
-// // Add Price
-// document.getElementsByTagName(
-//     "h4"
-// )[1].innerHTML = `Price: Rs <span style="color: #009688; font-weight: bold">${productData.price}</span>`;
-
-// // Add Description
-
-// document.getElementsByTagName(
-//     "h4"
-// )[2].innerHTML = `Description <br> <span style="margin-top: 20px;font-size: 16px;font-weight: 300;color: #86939e;">${productData.description}</span>`;
-
-// document.getElementsByTagName("h4")[3].innerHTML = `Product Preview`;
-
-// // Add Product Preview Pics
-
-// var productPics = document.createElement("div");
-// productPics.setAttribute("id", "productPics");
-// productDetails.appendChild(productPics);
-
-// var activeImg = "img0";
-// // add pics and set Event Click listenrs
-// productData.photos.forEach((item, i) => {
-//     var img = document.createElement("img");
-//     img.classList.add("imageStyle");
-//     img.setAttribute("id", `img${i}`);
-//     img.src = item;
-//     if (i == 0) img.classList.add("active");
-//     document.getElementById("productPics").appendChild(img);
-//     img.addEventListener("click", () => {
-//         document.getElementById(activeImg).classList.remove("active");
-//         img.classList.add("active");
-//         document.getElementById("productView").setAttribute("src", item);
-//         activeImg = `img${i}`;
-//     });
-// });
-
-// // add buy buton
-// var buy = document.createElement("input");
-// buy.setAttribute("type", "button");
-// buy.setAttribute("value", "Add to Cart");
-// buy.classList.add("buy-button");
-// buy.addEventListener("click", () => {
-//     window.alert(`${productData.name} is added to your cart`);
-// });
-// productDetails.appendChild(buy);
