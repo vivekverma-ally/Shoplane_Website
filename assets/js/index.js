683b65dcefa5976b34fcfc2b6d0e1eaa11b16c02
vivekverma-ally <85276140+vivekverma-ally@users.noreply.github.com>
2021-07-11 08:39:17 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,5 @@
 
 
-// async function getData(url) {
-
-//     // Storing response
-//     const response = await fetch(url);
-
-//     // Storing data in form of JSON
-//     var data = await response.json();
-//     console.log(data);
-//     console.log(typeof data)
-//     return data;
-// }
-// var productList = getData("https://5d76bf96515d1a0014085cf9.mockapi.io/product");
-
-
 var clothes = document.getElementById("clothingCards");
 sessionStorage.setItem("x", "uiosfuio");
 var accessory = document.getElementById("accessoriesCards");
@@ -25,7 +11,7 @@ fetch("https://5d76bf96515d1a0014085cf9.mockapi.io/product")
     productData = data;
     productList.forEach((item, i) => {
       var render = `<div class="card">
-            <a id="item${i}" href="./productDetail.html">
+            <a id="item${i}" href="https://vivekverma-ally.github.io/Shoplane_Website/productDetail.html?p=${item.id}">
               <div class="img">
                 <img src="` + item.preview + `" />
               </div>
@@ -40,10 +26,6 @@ fetch("https://5d76bf96515d1a0014085cf9.mockapi.io/product")
         accessory.innerHTML += render;
       if (!item.isAccessory)
         clothes.innerHTML += render;
-      // var link = document.getElementById(`item${i}`);
-      // link.addEventListener('click', event => {
-      //   sessionStorage.setItem("item", item);
-      // })
     })
   });
 
